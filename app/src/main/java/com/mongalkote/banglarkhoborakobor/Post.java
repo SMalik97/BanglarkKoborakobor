ad5386c2e4b666f51a4a63c707521460a44976cf
subrata <subratamalik1997@gmail.com>
2019-07-29 01:38:29 +0530

    add recycle view

--- app/src/main/java/com/mongalkote/banglarkhoborakobor/Post.java
+++ app/src/main/java/com/mongalkote/banglarkhoborakobor/Post.java
@@ -384,7 +384,7 @@ public class Post extends AppCompatActivity {
                     rurl = "https://www.mongalkote.com/wp-json/wp/v2/posts?categories=66&per_page=4";
                     break;
                 case "onno":
-                    rurl = "https://www.mongalkote.com/wp-json/wp/v2/posts?categories=68&per_page=4";
+                    rurl = "https://www.mongalkote.com/wp-json/wp/v2/posts?categories=64&per_page=4";
                     break;
             }
 
