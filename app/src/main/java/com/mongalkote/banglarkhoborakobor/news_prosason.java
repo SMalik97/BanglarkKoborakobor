eaee710746e48f3e0a31be4840cd223c7fbe6a3e
subrata <subratamalik1997@gmail.com>
2019-07-29 02:43:20 +0530

    show news on recycle item click

--- app/src/main/java/com/mongalkote/banglarkhoborakobor/news_prosason.java
+++ app/src/main/java/com/mongalkote/banglarkhoborakobor/news_prosason.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -15,6 +16,9 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -53,8 +57,33 @@ public class news_prosason extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_news_police, null);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_prosason, container, false);
+        card1=(CardView)view.findViewById(R.id.card1);
+        card1.setVisibility(View.INVISIBLE);
+        newsImage1=(ImageView)view.findViewById(R.id.newsImage1);
+        newsTitle1=(TextView)view.findViewById(R.id.newsTitle1);
+
+        //start loading news.....................................
+        loader = (DotsLoaderView)view.findViewById(R.id.dotload);
+        loader.show();
+//load first 15 news
+        //first10news f10n=new first10news();
+       // new Thread(f10n).start();
+
+        Date c= Calendar.getInstance().getTime();
+        SimpleDateFormat df=new SimpleDateFormat("dd-MMM-yyyy");
+        d=df.format(c);
+
+        recyclerView=(RecyclerView)view.findViewById(R.id.recyclerView);
+        layoutManager=new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+
+
+
+
+        return view;
     }
 
 }
